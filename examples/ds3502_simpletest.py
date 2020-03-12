c3cdcaf04cf5b373608ef690486beb79119ac1fc
dherrada <dylan.herrada@gmail.com>
2020-03-12 19:29:34 -0400

    Ran black, updated to pylint 2.x

--- examples/ds3502_simpletest.py
+++ examples/ds3502_simpletest.py
@@ -14,28 +14,28 @@ wiper_output = AnalogIn(board.A0)
 while True:
 
     ds3502.wiper = 127
-    print("Wiper set to %d"%ds3502.wiper)
+    print("Wiper set to %d" % ds3502.wiper)
     voltage = wiper_output.value
     voltage *= 3.3
     voltage /= 65535
-    print("Wiper voltage: %.2f V"%voltage)
+    print("Wiper voltage: %.2f V" % voltage)
     print("")
     sleep(1.0)
 
     ds3502.wiper = 0
-    print("Wiper set to %d"%ds3502.wiper)
+    print("Wiper set to %d" % ds3502.wiper)
     voltage = wiper_output.value
     voltage *= 3.3
     voltage /= 65535
-    print("Wiper voltage: %.2f V"%voltage)
+    print("Wiper voltage: %.2f V" % voltage)
     print("")
     sleep(1.0)
 
     ds3502.wiper = 63
-    print("Wiper set to %d"%ds3502.wiper)
+    print("Wiper set to %d" % ds3502.wiper)
     voltage = wiper_output.value
     voltage *= 3.3
     voltage /= 65535
-    print("Wiper voltage: %.2f V"%voltage)
+    print("Wiper voltage: %.2f V" % voltage)
     print("")
     sleep(1.0)
